26d1d8e76a4d8dc9f96e29736b2202b3def5dc11
JableDoge <129697099+JableDoge@users.noreply.github.com>
2023-04-06 16:45:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 707776914 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    s = np.sum(x)
+    estimate = 2 * s / (n * 56**2)
+    return estimate
